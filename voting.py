7e40f9e6ddc46caabfb0e83c2a5cb0c72481fd78
Choice Coin <87402354+ChoiceCoin@users.noreply.github.com>
2021-07-22 20:51:30 -0400

    Update voting.py

--- voting.py
+++ voting.py
@@ -1,3 +1,10 @@
+#This defines voting functions using traditional majority, STAR, and approval protocols.
+#All of the protocols below use Choice Coin to both quantify and send votes.
+#The funcions also define the ability to create accounts for an arbitary amount of voters.
+#The protocols below are minimum representations for two candidates, decisions,etc.
+#Read the Docs to learn how to add more candiates, decisions, and how to implement this protocol 
+#into you governance system.
+
 from algosdk import account, encoding, mnemonic,transaction
 from algosdk.future.transaction import AssetTransferTxn, PaymentTxn
 from algosdk.v2client import algod
@@ -46,8 +53,8 @@ def count(address):
 
 
 
-
-
+#This is quantifies a voter's vote and allows its to be recorded on the Algorand Blockchain through Choice Coin.
+#Each of the candiates/decisions will have their own Algorand Accounts to recieve these votes in the form of Choice Coin.
 def choice_vote(sender, key, receiver,amount):
     parameters = algod_client.suggested_params()
     transaction = AssetTransferTxn(sender, parameters, receiver, amount, choice_id)
@@ -58,6 +65,8 @@ def choice_vote(sender, key, receiver,amount):
     #Sends the transaction with the signature
     final = transaction.get_txid()
     return True, final
+
+
 
 def create_optin(receiver_mnemonic,receiver_address,index):
     parameters = algod_client.suggested_params()
@@ -70,7 +79,7 @@ def create_optin(receiver_mnemonic,receiver_address,index):
     return True
 
 
-
+#This allows the organization to generate an arbitary amount of Algorand Accounts for its voters.
 def initiate_new_accounts():
     x = input("How many voters do you need?")
     x = int(x)
@@ -82,6 +91,8 @@ def initiate_new_accounts():
         print("Address: {}\nPasscode: \"{}\"".format(public, passphrase))
 
 
+        
+#This defines a basic majority scheme on the Algorand Blockchain using the choice_vote function described earlier.       
 def new_vote(vote,address,t_mnemonic):
     private_key = mnemonic.to_private_key(mnemonic)
     if vote == "Yes":
@@ -91,7 +102,11 @@ def new_vote(vote,address,t_mnemonic):
     else:
         print("You did not submit the proper input")
 
-
+#This defines a basic Star_Voting scheme using Choice Coin. Users are allowed to rate
+#each candidate. Once they have done this, the choice_vote function will
+#send the appropriate allocation of Choice Coin to the winner account.
+#Read the docs to create a restriction on how many start per voter
+#are possible.
 def star_voting(address,t_mnemonic,candidate_rating,candiate_type):
     if candiate_type == 'one':
         candiate_rating = int(candidate_rating)
@@ -105,7 +120,9 @@ def star_voting(address,t_mnemonic,candidate_rating,candiate_type):
         choice_vote(address,private_key,decision_one,amount)
     print("Ballot Tabulated")
 
-
+#This defines a basic approval voting system. Again, this allows the voter 
+#to select all the decisons/candidates they approve of. This again only has two decisions.
+#Read our Docs to find out more!
 def approval_voting(address,t_mnemonic):
     first = input("Do you approve of first decision? Please answer using Yes or No")
     second = input("Do you approve of second decision? Please answer using Yes or No")
@@ -115,7 +132,8 @@ def approval_voting(address,t_mnemonic):
         choice_vote(address,private_key,decision_one,100)
     elif second == 'yes':
         choice_vote(address,private_key,decision_two,100)
-
+#This is a stateful smart contract to count the number of votes each candiate recieved
+#on the Algorand Blockchain.
 def count_votes():
     yes_count = check_holdings(decision_one)
     no_count = check_holding(decision_two)
