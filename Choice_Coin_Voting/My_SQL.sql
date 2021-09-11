a48cbe2d1d7f58f896a8be2d63e0739dfe4d55d6
gameline443 <64642750+gameline443@users.noreply.github.com>
2021-09-10 21:58:16 -0400

    Update My_SQL.sql
    Add tables for different voting classes

--- Choice_Coin_Voting/My_SQL.sql
+++ Choice_Coin_Voting/My_SQL.sql
@@ -14,3 +14,15 @@ name VARCHAR(100) NOT NULL,
 Stake INT(6),
 Secret VARCHAR(500) NOT NULL
 );
+CREATE table class
+(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+Stake INT(6),
+class VARCHAR(1) NOT NULL
+);
+CREATE table class_power
+(
+class VARCHAR(1) NOT NULL 
+votes_per_stake INT NOT NULL
+);
+
