6c7844c12cfd46f2bdc0535ec04924a839e7a1df
Choice Coin <87402354+ChoiceCoin@users.noreply.github.com>
2021-10-25 17:31:07 -0400

    Add files via upload

--- Choice_Coin_Voting/index.py
+++ Choice_Coin_Voting/index.py
@@ -1,34 +1,30 @@
+# Copyright Fortior Blockchain, LLLP 2021
 # Open Source under Apache License
-#To add additional decisions/candiates, add an additional boolean at line 128.
+   
 from flask import Flask, request, render_template, redirect, url_for
-from algorand_demo import algo_trade,choice_trade,create_optin,main_exchange
 from algosdk import account, encoding, mnemonic
 from vote import election_voting,hashing,corporate_voting,count_votes,count_corporate_votes
 from vote import reset_votes, reset_corporate_votes
 from algosdk.future.transaction import AssetTransferTxn, PaymentTxn
 from algosdk.v2client import algod
 import rsa
-from flaskext.mysql import MySQL
 import hashlib
+import sqlite3 as sl
 
+con = sl.connect('voters.db', check_same_thread = False)
+cur = con.cursor()
 app = Flask(__name__)
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = ''
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = ''
-app.config['MYSQL_DATABASE_HOST'] = ''
-mysql.init_app(app)
-conn = mysql.connect()
-cur = conn.cursor()
-
 finished = False
 corporate_finished = False
 validated = False
+
 
 @app.route("/")
 def start():
 	""" Start page """
 	return render_template('index.html')
+
+
 
 @app.route('/start', methods = ['POST', 'GET'])
 def start_voting():
@@ -37,26 +33,26 @@ def start_voting():
 	global finished
 	if request.method == 'POST':
 		key = hashing(str(request.form.get('Key')))
-		if key == '':
+		if key == '09a1d01b5b120d321de9529369640316ddb120870df1ec03b3f2c6dd39c1ff6ecf8de5e56eb32d79c9d06240eaf5de027f6e7b9df2e2e1a4cb38dd548460b757':
 			message = reset_votes()
 			finished = False
 		else:
 			error = "Incorrect admin key"
 	return render_template("start.html", message = message, error = error)
 
-@app.route('/corporatestart', methods = ['POST', 'GET'])
-def start_corporate():
-	error = ''
-	message = ''
-	global corporate_finished
+
+@app.route('/create', methods = ['POST','GET'])
+def create():
 	if request.method == 'POST':
-		key = hashing(str(request.form.get('Key')))
-		if key == '':
-			message = reset_corporate_votes()
-			corporate_finished = False
-		else:
-			error = "Incorrect admin key"
-	return render_template("start.html", message = message, error = error)
+		Social = hashing(str(request.form.get('Social')))
+		Drivers = hashing(str(request.form.get('Drivers')))
+		Key = hashing(str(request.form.get('Key')))
+		if str(Key) == '09a1d01b5b120d321de9529369640316ddb120870df1ec03b3f2c6dd39c1ff6ecf8de5e56eb32d79c9d06240eaf5de027f6e7b9df2e2e1a4cb38dd548460b757':
+			cur.execute("INSERT INTO USER (DL, SS) VALUES(?,?)",((Drivers,Social)))
+			con.commit()
+	return render_template('create.html')
+
+
 
 @app.route('/end', methods = ['POST','GET'])
 def end():
@@ -65,26 +61,18 @@ def end():
 	global finished
 	if request.method == 'POST':
 		key = hashing(str(request.form.get('Key')))
-		if key == '':
+		if key == '09a1d01b5b120d321de9529369640316ddb120870df1ec03b3f2c6dd39c1ff6ecf8de5e56eb32d79c9d06240eaf5de027f6e7b9df2e2e1a4cb38dd548460b757':
 			message = count_votes()
 			finished = True
 		else:
 			error = "Incorrect admin key"
 	return render_template("end.html", message = message, error = error)
 
-@app.route('/endcorporate', methods = ['POST', 'GET'])
-def corporate_end():
-	error = ''
-	message = ''
-	global corporate_finished
-	if request.method == 'POST':
-		key = hashing(str(request.form.get('Key')))
-		if key == '':
-			message = count_corporate_votes()
-			corporate_finished = True
-		else:
-			error = "Incorrect admin key"
-	return render_template('corporateend.html', message = message, error = error)
+
+
+
+
+
 
 @app.route('/vote', methods = ['POST','GET'])
 def vote():
@@ -95,11 +83,11 @@ def vote():
 	if request.method == 'POST':
 		Social = hashing(str(request.form.get('Social')))
 		Drivers = hashing(str(request.form.get('Drivers')))
-		cur.execute("SELECT * FROM accounts WHERE SS = %s AND DL = %s",(Social,Drivers))
+		cur.execute("SELECT * FROM USER WHERE SS = ? AND DL = ?",(Social,Drivers))
 		account = cur.fetchone()
 		if account:
-			cur.execute("DELETE FROM accounts WHERE SS = %s and DL = %s",(Social,Drivers))
-			conn.commit()
+			cur.execute("DELETE FROM USER WHERE SS = ? and DL = ?",(Social,Drivers))
+			con.commit()
 			validated = True
 			return redirect(url_for('submit'))
 		else:
@@ -108,6 +96,7 @@ def vote():
 		message = count_votes()
 		return render_template("end.html", message = message, error = error)
 	return render_template('vote.html', message = message, error = error)
+
 
 @app.route('/submit', methods = ['POST', 'GET'])
 def submit():
@@ -129,64 +118,14 @@ def submit():
 				 error = "You did not enter a vote"
 	return render_template('submit.html', message = message, error = error)
 
-@app.route('/corporate', methods = ['POST','GET'])
-def corporate():
-	error = ''
-	message = ''
-	if request.method == 'POST':
-		Key = hashing(str(request.form.get('Secret')))
-		Percentage = request.form.get('Stake')
-		vote = request.values.get("options")
-		cur.execute("SELECT * FROM corporate WHERE Secret = %s and Stake = %s",(Key,Percentage))
-		check = cur.fetchone()
-		if check:
-			if vote == 'option1':
-				vote = "YES"
-				message = corporate_voting(vote,Percentage)
-				cur.execute("DELETE FROM corporate WHERE Secret = %s and Stake = %s",(Key,Percentage))
-				conn.commit()
-			elif vote == 'option2':
-				vote = "NO"
-				message = corporate_voting(vote,Percentage)
-				cur.execute("DELETE FROM corporate WHERE Secret = %s and Stake = %s",(Key,Percentage))
-				conn.commit()
-			else:
-				error = "You did not enter a vote"
-		else:
-			error = "Your information was incorrect"
-	elif corporate_finished == True:
-		message = count_corporate_votes()
-		return render_template('end.html' ,message = message, error = error)
-	return render_template('corporate.html', message = message, error = error)
-
-@app.route('/create', methods = ['POST','GET'])
-def create():
-	if request.method == 'POST':
-		Name = request.form.get('Name')
-		Social = hashing(str(request.form.get('Social')))
-		Drivers = hashing(str(request.form.get('Drivers')))
-		Key = hashing(str(request.form.get('Key')))
-		if str(Key) == '':
-			cur.execute("INSERT INTO accounts (name, DL, SS) VALUES(%s,%s,%s)",((Name,Drivers,Social)))
-			conn.commit()
-	return render_template('create.html')
-
-@app.route('/corporate_creation',methods = ['POST','GET'])
-def corporate_create():
-	if request.method == 'POST':
-		Name = request.form.get('Name')
-		Key = hashing(str(request.form.get('Secret')))
-		Percentage = request.form.get('Stake')
-		Main = hashing(str(request.form.get('Main')))
-		if Main == "":
-			cur.execute("INSERT INTO corporate (name, Secret, Stake) VALUES(%s,%s,%s)",((Name,Key,Percentage)))
-			conn.commit()
-	return render_template('corporatecreate.html')
 
 @app.route('/about/')
 def about():
 	"""about"""
 	return render_template('about.html')
+
+
+
 
 if __name__ == "__main__":
 	app.run(host='127.0.0.1', debug=True)
