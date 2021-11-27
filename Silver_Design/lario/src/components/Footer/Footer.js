4eb1a9a8cc27f1151c4f86abfbf834778abe72f1
Lario <osunkunleolanrewaju@gmail.com>
2021-11-27 14:50:18 +0100

    added the frontend design

---
+++ Silver_Design/lario/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>
+            All Right Reserved @ 2021 Osunkunle Olanrewaju Olukunle
+            </p>
+        </div>
+    )
+}
+
+export default Footer
