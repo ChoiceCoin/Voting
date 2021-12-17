25edee766d1a30c74c6932f69857cd2d7da6ef9c
Choice Coin <87402354+ChoiceCoin@users.noreply.github.com>
2021-12-16 19:14:26 -0800

    Delete Shukuratu_Refinement Bash directory

--- Shukuratu_Refinement Bash/src/constants.js
+++
@@ -1,2 +0,0 @@
-const ASSET_ID = 21364625;
-export { ASSET_ID };
--- Shukuratu_Refinement Bash/src/store/stores.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-
-let initialState = {};
-
-const store = createStore(rootReducer, initialState);
-export default store;
