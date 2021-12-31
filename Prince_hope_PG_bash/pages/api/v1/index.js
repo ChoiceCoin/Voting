ac98c8cd4aa70eabaf9599232d37df73a62ceb22
Prince Charles Amachree <63968702+prince-hope1975@users.noreply.github.com>
2021-12-31 09:28:32 +0100

    Delete Prince_hope_PG_bash directory

--- Prince_hope_PG_bash/pages/api/v1/index.js
+++
@@ -1,6 +0,0 @@
-export default function handler (req,res){
-    if(req.method === !"GET"){
-        res.status("405").send({message:"Only post requests allowed"})
-    }
-    res.send("All Items")
-}
