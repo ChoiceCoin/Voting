8a3eb28b6ab3dd77aa0b2fcc718f5baf29589bfd
Prince Charles Amachree <63968702+prince-hope1975@users.noreply.github.com>
2021-12-31 02:54:45 +0100

    Proposals Generator Bash Submission
    The proposal generator contains multiple pages such as:
    Home page -  which displays the default proposals and new ones and houses multiple links to other pages
    Create Proposal Page - which uses a markdown editor to style different elements on the page, it supports links and tags
    View individual proposal - This allows the users to view already made proposals individually and cast votes
    
    The proposal generator also houses:
    - Connect and Disconnect wallet buttons; 
    - Support for AlgoSigner and WalletConnect;
    - Ability to edit Proposal if access is allowed
    - Protected Routes
    -- Delete Proposal, etc
    
    The generator was built with NEXT.js and tailwind.css

---
+++ Prince_hope_PG_bash/components/pages/proposal.js
@@ -0,0 +1,61 @@
+import MarkdownIt from "markdown-it"
+import { useState,useRef } from "react/cjs/react.development"
+import { useGlobalContext } from "../context"
+import Link from "next/link"
+const md = new MarkdownIt()
+
+const Proposal = ()=>{
+    const reference = useRef(null)
+    const { propsObj, markdown, setMarkdown, setPropsObj, title,setTitle } = useGlobalContext();
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        const length = propsObj.length
+        const id = (propsObj.filter((props)=>props.id ==id).length > 0 )? length+1:length;
+        setPropsObj([...propsObj, {name:title, desc:markdown, id:id, }])
+        localStorage.setItem("proposals",JSON.stringify( [
+          ...propsObj,
+          { name: title, desc: markdown, id: id },
+        ]));
+        (reference.current.click())
+        
+    }
+    const handleChange = (e)=>{
+        setMarkdown(e.target.value)
+    }
+    const handleTitleChange =(e)=>{
+        console.log(e.target.value)
+    }
+    // const result = md.render("# Markdown it rules")
+    return (
+      <form className="p-0 border-2 border-gray-500 rounded outline-none min-h-min">
+        <div className="bg-blue-400 px-2 text-gray-50">
+          This is a Markdown Editor So any Valid MarkDown will be valid
+        </div>
+        <input
+          onChange={handleTitleChange}
+          type="text"
+          placeholder="Title of Proposal"
+          className="mb-2 border-gray-500 border-t-2 border-b-2 outline-none px-4 py-2 w-full"
+        />
+        <textarea
+          placeholder="Proposal Details"
+          onChange={(e) => handleChange(e)}
+          className={`m-5 w-96 h-full outline-none`}
+          name="markdown"
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <input
+          type="submit"
+          className="w-full p-2 text-center bg-blue-800 text-white cursor-pointer"
+          onClick={(e) => handleSubmit(e)}
+        />
+
+      
+          <a  href="/"ref={reference}></a>
+      
+      </form>
+    );
+}
+export default Proposal
